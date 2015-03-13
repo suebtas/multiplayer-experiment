e4d36a56ee2c443fc6b538136c50548df6ef7a11
vbodurov <vladimir@bodurov.com>
2015-03-13 03:17:31 -0700

    Visual Studio

---
+++ CircuitRacerBegin/Assets/MenuStuff/MainMenuScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	
+	public Texture2D[] buttonTextures;
+    private float buttonWidth;
+    private float buttonHeight;	
+	
+	void OnGUI() {
+		for (int i = 0; i < 2; i++) {
+			if (GUI.Button (new Rect ((float)Screen.width * 0.5f - (buttonWidth / 2),
+			                          (float)Screen.height * (0.6f + (i * 0.2f)) - (buttonHeight / 2),
+			                          buttonWidth,
+			                          buttonHeight), buttonTextures[i])) {
+				Debug.Log("Mode " + i + " was clicked!");
+
+				if (i == 0) {
+					// Single player mode!
+					RetainedUserPicksScript.Instance.multiplayerGame = false;
+					Application.LoadLevel("PickCarMenu");
+				} else if (i == 1) {
+					RetainedUserPicksScript.Instance.multiplayerGame = true;
+                    Debug.Log("We would normally load a multiplayer game here");
+                }
+			}
+		}
+	}
+    
+    void Start() {
+        
+        // I know that 301x55 looks good on a 660-pixel wide screen, so we can extrapolate from there
+        buttonWidth = 301.0f * Screen.width / 660.0f;
+        buttonHeight = 55.0f * Screen.width / 660.0f;
+        
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }       
+}
---
+++ CircuitRacerBegin/Assets/Scripts/GameController.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject myCar;
+	public GuiController guiObject;
+	public GUISkin guiSkin;
+	public GameObject background;
+	public Sprite[] backgroundSprites;
+	public float[] startTimesPerLevel;
+	public int[] lapsPerLevel;
+
+	public bool _paused;
+	private float _timeLeft;
+	private float _timePlayed;
+	private int _lapsRemaining;
+	private bool _showingGameOver;
+	private bool _multiplayerGame;
+	private string gameOvertext;
+	private float _nextCarAngleTarget = Mathf.PI;
+	private const float FINISH_TARGET = Mathf.PI;
+
+	// Use this for initialization
+	void Start () {
+		RetainedUserPicksScript userPicksScript = RetainedUserPicksScript.Instance;
+		_multiplayerGame = userPicksScript.multiplayerGame;
+		if (! _multiplayerGame) {
+			// Can we get the car number from the previous menu?
+			myCar.GetComponent<CarController>().SetCarChoice(userPicksScript.carSelected, false);
+			// Set the background
+			background.GetComponent<SpriteRenderer>().sprite = backgroundSprites[userPicksScript.diffSelected - 1];
+			// Set our time left and laps remaining
+			_timeLeft = startTimesPerLevel[userPicksScript.diffSelected - 1];
+			_lapsRemaining = lapsPerLevel[userPicksScript.diffSelected - 1];
+
+			guiObject.SetTime (_timeLeft);
+			guiObject.SetLaps (_lapsRemaining);
+		} else {
+            SetupMultiplayerGame();
+		}
+
+	}
+
+    void SetupMultiplayerGame() {
+        // TODO: Fill this out!
+    }
+
+
+	void PauseGame() {
+		_paused = true;
+		myCar.GetComponent<CarController>().SetPaused(true);
+	}
+	
+	void ShowGameOver(bool didWin) {
+		gameOvertext = (didWin) ? "Woo hoo! You win!" : "Awww... better luck next time";
+		PauseGame ();
+		_showingGameOver = true;
+		Invoke ("StartNewGame", 3.0f);
+	}
+
+	void StartNewGame() {
+		Application.LoadLevel ("MainMenu");
+	}
+
+	void OnGUI() {
+		if (_showingGameOver) {
+			GUI.skin = guiSkin;
+			GUI.Box(new Rect(Screen.width * 0.25f, Screen.height * 0.25f, Screen.width * 0.5f, Screen.height * 0.5f), gameOvertext);
+
+		}
+	}
+    
+    
+    void DoMultiplayerUpdate() {
+        // In a multiplayer game, time counts up!
+        _timePlayed += Time.deltaTime;
+        guiObject.SetTime(_timePlayed);
+        
+        // We will be doing more here
+        
+    }
+	
+	void Update () {
+		if (_paused) {
+			return;
+		}
+
+		if (_multiplayerGame) {
+            DoMultiplayerUpdate();
+		} else {
+			_timeLeft -= Time.deltaTime;
+			guiObject.SetTime (_timeLeft);
+			if (_timeLeft <= 0) {
+				ShowGameOver (false);
+			}
+		}
+
+		float carAngle = Mathf.Atan2 (myCar.transform.position.y, myCar.transform.position.x) + Mathf.PI;
+		if (carAngle >= _nextCarAngleTarget && (carAngle - _nextCarAngleTarget) < Mathf.PI / 4) {
+			_nextCarAngleTarget += Mathf.PI / 2;
+			if (Mathf.Approximately(_nextCarAngleTarget, 2*Mathf.PI)) _nextCarAngleTarget = 0;
+			if (Mathf.Approximately(_nextCarAngleTarget, FINISH_TARGET)) {
+				_lapsRemaining -= 1;
+				Debug.Log("Next lap finished!");
+				guiObject.SetLaps (_lapsRemaining);
+				myCar.GetComponent<CarController>().PlaySoundForLapFinished();
+				if (_lapsRemaining <= 0) {
+					if (_multiplayerGame) {
+						// TODO: Properly finish a multiplayer game
+					} else {
+						ShowGameOver(true);
+					}
+				}
+			}
+		}
+
+	}
+}
