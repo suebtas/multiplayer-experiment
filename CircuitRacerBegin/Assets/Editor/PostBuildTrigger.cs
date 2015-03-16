4a758622ccd28be5835f9862f00c7e7f6c6eb27d
vbodurov <vladimir@bodurov.com>
2015-03-15 20:01:25 -0700

    postbuild updated - try to fix file access #6

--- CircuitRacerBegin/Assets/Editor/PostBuildTrigger.cs
+++ CircuitRacerBegin/Assets/Editor/PostBuildTrigger.cs
@@ -45,11 +45,11 @@ namespace Assets.Editor
 69AC98DA1AB530AB006C7BE9;libc++.dylib;Frameworks;69AC98D21AB530AB006C7BE9;""usr/lib/libc++.dylib"";SDKROOT;PBXBuildFile|PBXFileReference|PBXFrameworksBuildPhase|PBXGroupFrameworks;""compiled.mach-o.dylib""
 69AC98DB1AB530AB006C7BE9;libz.dylib;Frameworks;69AC98D31AB530AB006C7BE9;usr/lib/libz.dylib;SDKROOT;PBXBuildFile|PBXFileReference|PBXFrameworksBuildPhase|PBXGroupFrameworks;""compiled.mach-o.dylib""
 69AC98DC1AB530AB006C7BE9;Security.framework;Frameworks;69AC98D41AB530AB006C7BE9;System/Library/Frameworks/Security.framework;SDKROOT;PBXBuildFile|PBXFileReference|PBXFrameworksBuildPhase|PBXGroupFrameworks;wrapper.framework
-69AC98DE1AB54133006C7BE9;gpg.framework;Frameworks;69AC98DD1AB54133006C7BE9;../../../../GoogleLibs/gpg.framework;<group>;PBXBuildFile|PBXFileReference|PBXFrameworksBuildPhase|PBXGroupCustomTemplate;wrapper.framework
-69AC98E01AB5413C006C7BE9;GooglePlus.framework;Frameworks;69AC98DF1AB5413C006C7BE9;../../../../GoogleLibs/GooglePlus.framework;<group>;PBXBuildFile|PBXFileReference|PBXFrameworksBuildPhase|PBXGroupCustomTemplate;wrapper.framework
-69AC98E21AB54143006C7BE9;GooglePlus.bundle;Resources;69AC98E11AB54143006C7BE9;../../../../GoogleLibs/GooglePlus.bundle;<group>;PBXBuildFile|PBXFileReference|PBXGroupCustomTemplate|PBXResourcesBuildPhase;""wrapper.plug-in""
-69AC98E41AB5414C006C7BE9;GooglePlayGames.bundle;Resources;69AC98E31AB5414C006C7BE9;../../../../GoogleLibs/GooglePlayGames.bundle;<group>;PBXBuildFile|PBXFileReference|PBXGroupCustomTemplate|PBXResourcesBuildPhase;""wrapper.plug-in""
-69AC98E61AB54154006C7BE9;GoogleOpenSource.framework;Frameworks;69AC98E51AB54154006C7BE9;../../../../GoogleLibs/GoogleOpenSource.framework;<group>;PBXBuildFile|PBXFileReference|PBXFrameworksBuildPhase|PBXGroupCustomTemplate;wrapper.framework
+69AC98DE1AB54133006C7BE9;gpg.framework;Frameworks;69AC98DD1AB54133006C7BE9;../../../../GoogleLibs/gpg.framework;""<group>"";PBXBuildFile|PBXFileReference|PBXFrameworksBuildPhase|PBXGroupCustomTemplate;wrapper.framework
+69AC98E01AB5413C006C7BE9;GooglePlus.framework;Frameworks;69AC98DF1AB5413C006C7BE9;../../../../GoogleLibs/GooglePlus.framework;""<group>"";PBXBuildFile|PBXFileReference|PBXFrameworksBuildPhase|PBXGroupCustomTemplate;wrapper.framework
+69AC98E21AB54143006C7BE9;GooglePlus.bundle;Resources;69AC98E11AB54143006C7BE9;../../../../GoogleLibs/GooglePlus.bundle;""<group>"";PBXBuildFile|PBXFileReference|PBXGroupCustomTemplate|PBXResourcesBuildPhase;""wrapper.plug-in""
+69AC98E41AB5414C006C7BE9;GooglePlayGames.bundle;Resources;69AC98E31AB5414C006C7BE9;../../../../GoogleLibs/GooglePlayGames.bundle;""<group>"";PBXBuildFile|PBXFileReference|PBXGroupCustomTemplate|PBXResourcesBuildPhase;""wrapper.plug-in""
+69AC98E61AB54154006C7BE9;GoogleOpenSource.framework;Frameworks;69AC98E51AB54154006C7BE9;../../../../GoogleLibs/GoogleOpenSource.framework;""<group>"";PBXBuildFile|PBXFileReference|PBXFrameworksBuildPhase|PBXGroupCustomTemplate;wrapper.framework
         ";
 
             var frameworks = data.Trim()
